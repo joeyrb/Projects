a477008452352f87b74aa44d3de367c662010f69
joey <joeyrb93@gmail.com>
2017-11-25 23:36:40 -0700

    changes to gui to accept keyboard input

--- Python/gui.py
+++ Python/gui.py
@@ -9,11 +9,70 @@ https://docs.python.org/3/library/tkinter.html#tkinter-modules
 Turtle:
 https://docs.python.org/3/library/turtle.html#module-turtle
 '''
+try:
+	# for Python2
+	from Tkinter import *
+except ImportError:
+	# for Python3
+	from tkinter import *
 
 
-import tkinter
+def hello(event):
+	print("Single Click, Button-1")
 
-# 
+def quit(event):
+	print("Double Click, stopping...")
+	import sys; sys.exit()
+
+def motion(event):
+	print("Mouse position: (%s, %s)" % (event.x, event.y))
+	return
+
+def getNext(event):
+	print("Right Key Pressed = NEXT")
+
+def getPrev(event):
+	print("Left key pressed = PREVIOUS")
+
+def key(event):
+	print "pressed", repr(event.char)
+
+def leftKey(event):
+	getPrev(event)
+
+def rightKey(event):
+	getNext(event)
+
+def main():
+	root = Tk()
+	frame = Frame(root, width=640, height=480)
+
+	widget = Button(root, text='Mouse Clicks')
+	widget.pack()
+	widget.bind('<Button-1>', hello)
+	widget.bind('<Double-1>', quit)
+
+	next_btn = Button(root, text='NEXT>', fg='green')
+	next_btn.bind('<Button-1>', getNext)
+	next_btn.pack()
+
+	prev_btn = Button(root, text = '<PREV', fg='blue')
+	prev_btn.bind('<Button-1>', getPrev)
+	prev_btn.pack()
+
+	frame.bind('<Key>', key)
+	root.bind('<Left>', leftKey)
+	root.bind('<Right>', rightKey)
+	root.bind('<Motion>', motion)
+	frame.pack()
+	frame.mainloop()
+
+
+if __name__ == '__main__':
+	main()
+
+
+# TURTLE TUTORIAL
 # from turtle import *
 # color('yellow', 'blue')
 # begin_fill()
@@ -24,6 +83,3 @@ import tkinter
 #         break
 # end_fill()
 # done()
-
-top = tkinter.Tk()
-top.mainloop()
